b0a31b5289bc198efedfd4ef21fb6107dee4a3ca
bart016 <bart.turczynski@gmail.com>
2021-09-25 20:41:04 +0200

    komit dla piotrka

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform transform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.p += 0.1;
+    }
+}
