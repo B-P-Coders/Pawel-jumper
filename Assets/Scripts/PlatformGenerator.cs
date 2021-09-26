15e9121985ef1749e6546db63ad996f7e74da31a
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-09-26 16:38:55 +0200

    Sterowanie kamery, generowanie platform i początek skakania

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] Levels = new Transform[3];
+    [SerializeField]
+    private GameObject[] Platforms = new GameObject[3];
+    private Transform transform;
+    private Vector3 startPosition;
+    [SerializeField]
+    private Vector3 distance;
+    [SerializeField]
+    private Transform placeholder;
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (startPosition.x + distance.x < transform.position.x) generatePlatform();
+    }
+
+    private void generatePlatform()
+    {
+        int platformId = Random.Range(0, 2);
+        int levelId = Random.Range(0, 2);
+        Instantiate(Platforms[platformId], Levels[levelId].position, Quaternion.identity, placeholder);
+        startPosition = transform.position;
+    }
+}
